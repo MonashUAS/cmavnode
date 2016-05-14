d87bb9ad56d7871ce875aa75afdc004f565b7a44
james <jamesmare12@gmail.com>
2016-05-14 20:24:56 +1000

    constructor to initialise port and ioservice

--- src/serial.cpp
+++ src/serial.cpp
@@ -4,3 +4,36 @@
  * SERIAL CLASS
  * This class extends 'link' and overrides it methods to handle uart communications
  */
+
+#include "serial.h"
+
+serial::serial(const std::string& port, 
+        const std::string& baudrate):
+    io_service_(), port_(io_service_){
+
+        //open the port with connection string
+        port_.open(port);
+
+
+        if(!port_.is_open()){
+            //do something fancy
+        }
+
+        //configure the port
+        port_.set_option(boost::asio::serial_port_base::baud_rate(std::stoi(baudrate)));
+        port_.set_option(boost::asio::serial_port_base::flow_control(
+                    boost::asio::serial_port_base::flow_control::none));
+        port_.set_option(boost::asio::serial_port_base::parity(
+                    boost::asio::serial_port_base::parity::none));
+        port_.set_option(boost::asio::serial_port_base::stop_bits(
+                    boost::asio::serial_port_base::stop_bits::one));
+
+}
+
+void serial::runWriteThread(){
+    //stub
+}
+
+void serial::runReadThread(){
+    //stub
+}
--- src/serial.h
+++ src/serial.h
@@ -7,11 +7,37 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
-class serial: mlink
+#include <string>
+#include <boost/asio.hpp>
+
+#include "mlink.h"
+
+class serial: public mlink
 {
     public:
+        //construct and destruct
+        serial(const std::string& port,
+                const std::string& baudrate);
+        ~serial();
 
+        //override virtuals from mlink
+        void runWriteThread();
+        void runReadThread();
     private:
+        //Callbacks for async send/recv
+        void handle_receive_from(const boost::system::error_code& error,
+                size_t bytes_recvd);
+        void handle_send_to(const boost::system::error_code& error,
+            size_t bytes_recvd);
+
+	boost::asio::io_service io_service_;
+        boost::asio::serial_port port_;
+
+        //takes message, puts onto buff and calls send
+        void processAndSend(mavlink_message_t *msgToConvert);
+
+        //Actually sends
+        void send(uint8_t *buf, std::size_t buf_size);
 
 };
 
