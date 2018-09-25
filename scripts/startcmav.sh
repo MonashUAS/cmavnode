65e1bc80ce3c683f9bd70d6dd4d64dae42053ab3
James Mare <git@jmare.me>
2018-09-25 16:11:46 +1000

    improved restart scripts and added air restart script

--- scripts/startcmav.sh
+++ scripts/startcmav.sh
@@ -3,5 +3,8 @@
 python loadconfig.py ../example/ground.json &
 ../build/cmavnode -H 8000 -r ~/catkin_ws/src/New_GUI/media/roi
 
-python loadconfig.py ../example/groundsafe.json &
-../build/cmavnode -H 8000 -r ~/catkin_ws/src/New_GUI/media/roi
+while true; do
+	sleep 1
+	python loadconfig.py ../example/groundsafe.json &
+	../build/cmavnode -H 8000 -r ~/catkin_ws/src/New_GUI/media/roi
+done
